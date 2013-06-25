21836bbcfe615c38b15de8ef0dd32818f37740d8
Julian <julian@nairobi.(none)>
2013-06-25 19:24:56 -0300

    [IMP] Almost working shell v0.2.1

---
+++ test/cat.c
@@ -0,0 +1,24 @@
+#include "syscall.h"
+
+int main(int argc, char* argv[]) {
+    OpenFileId fileId;
+    int i;
+    char ch;
+
+    if (argc == 1) {
+        while(1) {
+            Read(&ch, 1, ConsoleInput);
+            Write(&ch, 1, ConsoleOutput);
+            Write("\n", 1, ConsoleOutput);
+        }
+    } else {
+        for (i = 1; i <= argc; i++) {
+            Write("\n", 1, ConsoleOutput);
+            fileId = Open(argv[i]);
+            while (Read(&ch, 1, fileId) == 1) {
+                Write(&ch, 1, ConsoleOutput);
+            }
+            Close(fileId);
+        }
+    }
+}
